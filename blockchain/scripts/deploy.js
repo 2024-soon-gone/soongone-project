d77f10b0413bb76ae268aefaac85ecf707b043da
giirafe <seojune408@gmail.com>
2024-04-01 23:17:32 +0900

    erc20 contract <SoGo.sol> added

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -2,18 +2,27 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log('Deploying contracts with the account:', deployer.address);
 
-  // Get the ContractFactory for your contract
-  const SampleNFT = await ethers.getContractFactory('sampleNFT');
-
-  // Deploy the contract
-  const token = await SampleNFT.deploy(
+  // SoGo(ERC20) for SoonGone Deploy
+  const SoGoContract = await ethers.getContractFactory('SoGo');
+  const SoGo = await SoGoContract.deploy(
     '0xF9a2E8a6021b60Ea81a9CFA727cc22c8Baf07e54',
   );
+  // await SoGo.initialize('0xF9a2E8a6021b60Ea81a9CFA727cc22c8Baf07e54', {
+  //   gasLimit: 3000000,
+  // }); // Init with TestAccount 1 to SGNFT Deployer
+  console.log('SoGo Contract Address : ', SoGo.target);
+  // const SoGoSupply = await SoGo.balanceOf( //
+  //   '0xF9a2E8a6021b60Ea81a9CFA727cc22c8Baf07e54',
+  // );
+  // console.log('Initial SoGo mint amount : ', SoGoSupply);
 
-  // Wait for the contract to be deployed
-  //   await token.deployed();
+  // SGNFT Deploy
+  const SGNFTContract = await ethers.getContractFactory('SGNFT');
+  const SGNFT = await SGNFTContract.deploy(
+    '0xF9a2E8a6021b60Ea81a9CFA727cc22c8Baf07e54', // Set TestAccount 1 to SGNFT Deployer
+  );
 
-  console.log('NFT Contract Address :', token.target); // token.target == Contract deployed address
+  console.log('SGNFT Contract Address : ', SGNFT.target); // SGNFT.target == Contract deployed address
 }
 
 main()
