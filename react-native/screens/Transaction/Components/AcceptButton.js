a2791f406b65d7a378db92d9bc287cc84c4ff50a
ImSupporter <adam990901@gmail.com>
2024-10-15 19:15:59 +0900

    받은 제안 '수락' 버튼 버그 수정

--- react-native/screens/Transaction/Components/AcceptButton.js
+++ react-native/screens/Transaction/Components/AcceptButton.js
@@ -3,7 +3,17 @@ import { Pressable, StyleSheet, Text } from 'react-native';
 import theme from '../../../assets/Theme';
 function AcceptButton({ onPress }) {
   return (
-    <Pressable>
+    <Pressable
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 53,
+        height: 32,
+        borderRadius: 8,
+        backgroundColor: theme.black,
+      }}
+    >
       <Text style={styles.button} onPress={onPress}>
         수락
       </Text>
@@ -12,15 +22,8 @@ function AcceptButton({ onPress }) {
 }
 const styles = StyleSheet.create({
   button: {
-    display: 'flex',
-    width: 53,
-    height: 32,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8,
     color: theme.white,
     fontSize: 12,
-    backgroundColor: theme.black,
   },
 });
 
